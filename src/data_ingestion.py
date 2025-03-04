bc2be80b607c972c6224ea31c46f79bd76daabf4
Santanu211 <santanupal026@gmail.com>
2025-03-04 09:39:05 +0530

    add first exp

---
+++ src/data_ingestion.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import os
+
+
+df=pd.read_csv(r"c:\Users\Admin\Downloads\student_data_100.csv")
+print(df.head(1))
+
+df=df.drop(["Student_ID","Gender","Major"],axis=1)
+print(df.head(1))
+
+if "Credits_Completed" in df.columns:
+    # Drop NaN values before filtering
+    df = df[df["Credits_Completed"].notnull()]
+    
+    # Apply filter
+    df = df[df["Credits_Completed"] > 50]
+
+
+
+df.to_csv(os.path.join("data","student.csv"))
