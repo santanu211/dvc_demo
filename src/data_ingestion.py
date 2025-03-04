b92ede5262ca383307e3ea86a0b203d513227f27
Santanu211 <santanupal026@gmail.com>
2025-03-04 09:51:28 +0530

    add second expre

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -6,7 +6,7 @@ import os
 df=pd.read_csv(r"c:\Users\Admin\Downloads\student_data_100.csv")
 print(df.head(1))
 
-df=df.drop(["Student_ID","Gender","Major"],axis=1)
+df=df.drop(["Student_ID","Gender","Major","Age"],axis=1)
 print(df.head(1))
 
 if "Credits_Completed" in df.columns:
@@ -14,7 +14,7 @@ if "Credits_Completed" in df.columns:
     df = df[df["Credits_Completed"].notnull()]
     
     # Apply filter
-    df = df[df["Credits_Completed"] > 50]
+    df = df[df["Credits_Completed"] > 60]
 
 
 
